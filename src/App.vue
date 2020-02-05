280982825b592a7ac434f31174c3cf6694e76c25
HD-plus <60195758+HD-plus@users.noreply.github.com>
2020-02-05 23:26:50 +0100

    Update App.vue
    Added Kevin&Hermann

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App Kevin"/>
+    <HelloWorld msg="Welcome to Your Vue.js App brought to you by Kevin&Hermann"/>
   </div>
 </template>
 
