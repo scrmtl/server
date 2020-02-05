cddb8c686deb60e6bea03135b478a419e287d476
HD-plus <60195758+HD-plus@users.noreply.github.com>
2020-02-05 23:57:32 +0100

    Update App.vue
    Fixed special character

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App brought to you by Kevin&Hermann"/>
+    <HelloWorld msg="Welcome to Your Vue.js App brought to you by Kevin und Hermann"/>
   </div>
 </template>
 
