63cbfef54bd9ac9fcf8374d919db8cb81fb6d459
HD-plus <60195758+HD-plus@users.noreply.github.com>
2020-02-06 17:59:20 +0100

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      This is a test for CI/CD
+      This is a test for CI/CD <br>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
