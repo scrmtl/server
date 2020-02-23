9eaa2e0b6deacdecc0ef280c3f3edc7608179435
Stephan Führer <s.fuehrer@ctsgmbh.local>
2020-02-23 21:28:11 +0100

    v.0.0.1.1 slug auskommentiert bei models(SprintBacklog) - damit Adminbereich funktioniert

--- scrum-tool/scrumtool/scrumtoolHome/models.py
+++ scrum-tool/scrumtool/scrumtoolHome/models.py
@@ -11,12 +11,12 @@ class SprintBacklog (models.Model):
     productBacklog = models.ForeignKey(to='ProductBacklog', on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
-    slug = models.SlugField(unique=True)
+    #slug = models.SlugField(unique=True)
     def __str__(self):
         return "{0} ({1})".format(self.name, self.description)
 
 class TaskCard (models.Model):
-    productBacklog = models.ForeignKey(to='SprintBacklog', on_delete=models.CASCADE)
+    sprintBacklog = models.ForeignKey(to='SprintBacklog', on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
     def __str__(self):
