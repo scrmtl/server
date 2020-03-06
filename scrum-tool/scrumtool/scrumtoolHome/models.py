40eef55a6739e6dae0a2052ea4d88589a2d07f79
HD-plus <dobehe78493@th-nuernberg.de>
2020-03-07 00:11:43 +0100

    added models

--- scrum-tool/scrumtool/scrumtoolHome/models.py
+++ scrum-tool/scrumtool/scrumtoolHome/models.py
@@ -6,19 +6,25 @@ class ProductBacklog (models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
     slug = models.SlugField(unique=True)
+
     def __str__(self):
         return "{0} ({1})".format(self.name, self.slug)
 
+
 class SprintBacklog (models.Model):
-    productBacklog = models.ForeignKey(to='ProductBacklog', on_delete=models.CASCADE)
+    productBacklog = models.ForeignKey(
+        to='ProductBacklog', on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
     #slug = models.SlugField(unique=True)
+
     def __str__(self):
         return "{0} ({1})".format(self.name, self.description)
 
+
 class TaskCard (models.Model):
-    sprintBacklog = models.ForeignKey(to='SprintBacklog', on_delete=models.CASCADE)
+    sprintBacklog = models.ForeignKey(
+        to='SprintBacklog', on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
     storypoints = models.IntegerField(default=0)
@@ -28,3 +34,20 @@ class TaskCard (models.Model):
 
     def __str__(self):
         return "{0} ({1}) {2} {3} {4} {5} {5}".format(self.name, self.description, self.storypoints, self.label_backend, self.label_frontend, self.label_other)
+
+
+class Checklist (models.Model):
+    taskCard = models.ForeignKey(to='TaskCard', on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return "{0} ".format(self.name)
+
+
+class ChecklistItem (models.Model):
+    checklist = models.ForeignKey(to='Checklist', on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    checked = models.BooleanField()
+
+    def __str__(self):
+        return "{0}: {1} ".format(self.name, self.checked)
