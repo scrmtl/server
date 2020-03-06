e853bee7a6d4dd8ed7225a0fc4d2e1abaada89f8
HD-plus <dobehe78493@th-nuernberg.de>
2020-03-07 00:02:42 +0100

    added html

--- scrum-tool/scrumtool/scrumtoolHome/urls.py
+++ scrum-tool/scrumtool/scrumtoolHome/urls.py
@@ -3,9 +3,10 @@ from .views import index, login, pb, sb
 
 
 urlpatterns = [
-        #wenn Anfrage reinkommt, dann übergebe das der Funktion index aus der views.py
-        path ('', login, name='login.html'),
-        path ('home', index, name='index.html'),
-        path ('pb', pb, name='pb.html'),
-        path ('sb', sb, name='sb.html'),
+    # wenn Anfrage reinkommt, dann übergebe das der Funktion index aus der views.py
+    path('', login, name='login.html'),
+    path('home', index, name='index.html'),
+    path('pb', pb, name='pb.html'),
+    path('sb', sb, name='sb.html'),
+    path('checklist', checklist, name="checklist.html")
 ]
--- scrum-tool/scrumtool/scrumtoolHome/views.py
+++ scrum-tool/scrumtool/scrumtoolHome/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ProductBacklog, SprintBacklog, TaskCard
 
+
 def index(request):
-    #return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
     context = {'backlog': ProductBacklog.objects.all(),
-                'sb': SprintBacklog.objects.all(),
-                'tasks': TaskCard.objects.all(),
-                }
+               'sb': SprintBacklog.objects.all(),
+               'tasks': TaskCard.objects.all(),
+               }
 
     return render(request=request, template_name='scrumtool/index.html',
-                    context=context)
+                  context=context)
 
 
 def login(request):
@@ -20,16 +21,25 @@ def login(request):
 
 def pb(request):
     context = {'backlog': ProductBacklog.objects.all(),
-                }
+               }
 
     return render(request=request, template_name='scrumtool/pb.html',
-                    context=context)
+                  context=context)
+
 
 def sb(request):
     context = {'backlog': ProductBacklog.objects.all(),
-                'sb': SprintBacklog.objects.all(),
-                'tasks': TaskCard.objects.all(),
-                }
+               'sb': SprintBacklog.objects.all(),
+               'tasks': TaskCard.objects.all(),
+               }
 
     return render(request=request, template_name='scrumtool/sb.html',
-                    context=context)
+                  context=context)
+
+
+def checklist(request):
+    context = {
+
+    }
+    return render(request=request, template_name='scrumtool/checklist.html',
+                  context=context)
