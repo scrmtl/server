909d2eb08c3038a6c865c8e35e629cb46694cbc5
HD-plus <dobehe78493@th-nuernberg.de>
2020-04-17 17:34:24 +0200

    added StorypointHandler

--- backend/scrumtool/api/signals/storypoints.py
+++ backend/scrumtool/api/signals/storypoints.py
@@ -1,17 +1,25 @@
 """After card is saves calculate all values together
 """
 import logging
-from django.db.models.signals import post_save
+import sys
+from django.db.models.signals import post_save, pre_save, pre_delete
 from django.dispatch import receiver
+from ..models import Task, Feature, Epic, Card
 
 logging.basicConfig(level=logging.DEBUG)
 stdlogger = logging.getLogger(__name__)
 
 
-@receiver(post_save, sender='api.Task')
+@receiver(pre_save, sender='api.Task')
+@receiver(pre_delete, sender='api.Task')
 def run_after_saving_task(sender, **kwargs):
-    stdlogger.info("Task changed")
+    """Is called when task changes.
+    Handels changes in Storypoints.
+    """
 
+    stdlogger.info("Task changed")
+    str_pt_hdl = StorypointHandler(sender)
+    str_pt_hdl.calculateStorypoints()
     _feature = sender.feature
     _epic = sender.feature.epic
 
@@ -32,3 +40,31 @@ def run_after_saving_epic(sender, **kwargs):
     stdlogger.info("sender class %s" % (sender.__name__))
     stdlogger.info("sender %s" % (sender))
     stdlogger.info("kwargs %s" % str(kwargs))
+
+
+class StorypointHandler():
+    def __init__(self, card):
+        print(type(Task))
+        print(type(card))
+        print(isinstance(card, 'Task'))
+        if isinstance(card, (Task, Feature, Task)):
+            self.card = card
+        else:
+            raise TypeError(
+                '{card} is not of datatype Card'.format(card=repr(card)))
+        self.task = None
+        self.feature = None
+        self.epic = None
+
+    def calculateStorypoints(self):
+        self.__fill_card_classes()
+
+    def __fill_card_classes(self):
+        if isinstance(self.card, Task):
+            self.task = self.card
+            self.feature = self.task.feature
+            self.epic = self.feature.epic
+        if isinstance(self.card, Feature):
+            pass
+        if isinstance(self.card, Epic):
+            pass
