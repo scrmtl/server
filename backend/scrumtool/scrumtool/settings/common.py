0e21b614877a6e553491752900b90d4bf9e6fb6d
HD-plus <dobehe78493@th-nuernberg.de>
2020-04-23 12:36:26 +0200

    Added corsheaders middleware

--- backend/scrumtool/scrumtool/settings/common.py
+++ backend/scrumtool/scrumtool/settings/common.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'scrumtool.urls'
