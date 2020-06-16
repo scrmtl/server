01b7bfedee27165081b39416293edc8d76e6dc45
HD-plus <dobehe78493@th-nuernberg.de>
2020-06-16 21:45:45 +0200

    Changed settings to production

--- backend/scrumtool/scrumtool/asgi.py
+++ backend/scrumtool/scrumtool/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrumtool.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'scrumtool.settings.production')
 
 application = get_asgi_application()
