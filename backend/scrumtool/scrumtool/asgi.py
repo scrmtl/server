3ea3a7a2ada331a912d07fad2c0f0b3b9becce57
HD-plus <dobehe78493@th-nuernberg.de>
2020-06-17 17:17:27 +0200

    removed old migrations

--- backend/scrumtool/scrumtool/asgi.py
+++ backend/scrumtool/scrumtool/asgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'scrumtool.settings.production')
+                      'scrumtool.settings.development')
 
 application = get_asgi_application()
