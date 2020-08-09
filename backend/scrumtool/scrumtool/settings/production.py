897d0a21e4ed9cf552087394810b0690e5f68e38
huskyre <60154287+husKyRe@users.noreply.github.com>
2020-08-09 14:41:37 +0200

    refactoring enviroment vars for common and production.py. Setup allowed_hosts in django settings also as env.
    add in docker-compose.yml all env

--- backend/scrumtool/scrumtool/settings/production.py
+++ backend/scrumtool/scrumtool/settings/production.py
@@ -1,27 +1,15 @@
 import os
 from scrumtool.settings.common import *
 
-
-POSTGRES_HOST = os.getenv('POSTGRES_HOST',
-                          'postgres')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD',
-                              'postgresistdasbestedockerimage')
-POSTGRES_USER = os.getenv('POSTGRES_USER',
-                          'scrmtladmin')
-POSTGRES_DB = os.getenv('POSTGRES_DB',
-                        'scrumtooldb')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT',
-                          '5432')
-
 DEBUG = True
 
 # SECRET_KEY = os.environ['SECRET_KEY']
 SECRET_KEY = '725#n#v8bw-nw8_pd3zcos)l-o13)ua4uju(64pbj5z*8ryr(x'
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['scrmtl.ddns.net',
-                 'elherminius.ddns.net',
-                 'localhost']
+# 'BACKEND_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'BACKEND_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("BACKEND_ALLOWED_HOSTS").split(" ")
 
 # https://stackoverflow.com/questions/56916448/access-control-allow-origin-issue-in-vue-js-and-django
 CORS_ORIGIN_ALLOW_ALL = True
@@ -58,6 +46,6 @@ REST_FRAMEWORK = {
     )
 
 }
-
+# Needed for reverse proxy
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
