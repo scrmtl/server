0cbd4398234130ba2bf35a81e2db2acfce35a58d
Kevin <60154287+husKyRe@users.noreply.github.com>
2020-09-04 19:08:50 +0200

    Documentation
    Document vuex-crud generated getters and actions

--- frontend/src/store/ressources/projectUser.js
+++ frontend/src/store/ressources/projectUser.js
@@ -2,4 +2,15 @@ import createCrudModule from 'vuex-crud';
 
 export default createCrudModule({
     resource: 'project_users', // The name of your CRUD resource (mandatory)
+    // Follow actions are generated:
+    // FETCH_LIST
+    // FETCH_SINGLE
+    // CREATE
+    // UPDATE
+    // REPLACE
+    // DESTROY
+    
+    // Follow getters are generated:
+    // list 
+    // byid(id)
 });
