9948c64009db975f8a55a8f3e87431f441108add
Dreamer <dreamer@online.de>
2020-09-05 23:03:52 +0200

    Hinzufügen der Lanes im Product Backlog

--- frontend/src/views/ProductBacklog.vue
+++ frontend/src/views/ProductBacklog.vue
@@ -2,8 +2,8 @@
   <v-content class="tabbody tab-content">
     <v-container fluid class="pl-10">
        <v-row>
-          <div>
-            
+          <div v-for="lane in laneList" :key="lane.id">
+            <Lane v-bind:lane="lane"></Lane>
           </div>
       </v-row>
     </v-container>
@@ -11,13 +11,53 @@
 </template>
 
 <script>
-
+import Lane from "@/components/Lane.vue";
+import { mapGetters, mapActions, mapState } from "vuex";
 
 export default {
+  data: () => ({}),
+  props: ["SprintBacklogBoard"],
   components: {
+    Lane
+  },
+  computed: {
+    ...mapGetters("lane", {
+      laneList: "list"
+    }),
 
+    ...mapState([
+      "route" // vuex-router-sync
+    ])
+  },
+
+  methods: {
+    ...mapActions("lane", {
+      fetchLanes: "fetchList"
+    }),
+
+    fetchData() {
+      return this.fetchLanes({customUrlFnArgs: this.SprintBacklogBoard.id});
+    }
+  },
+
+  watch: {
+    $route: "fetchData"
+  },
+
+  created() {
+    this.fetchData();
+  },
+  mounted: function() {
+    this.fetchData();
+    setInterval(
+      function() {
+        this.fetchData();
+      }.bind(this),
+      10000
+    );
   }
 };
+
 </script>
 
 <style lang="css" scoped>
