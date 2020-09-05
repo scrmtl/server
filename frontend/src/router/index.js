64b5a683416d051012edeb66ed90fe8544eb0c83
Kevin <60154287+husKyRe@users.noreply.github.com>
2020-09-05 22:45:04 +0200

    Set Router for project

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,12 +9,13 @@ import SprintPlaning from "../views/SprintPlaning.vue"
 import Statistic from "../views/Statistic.vue"
 import Archive from "../views/Archive.vue"
 
-import Lane from "../components/Lane.vue";
-import Epic from "../components/Epic.vue";
-import Feature from "../components/Feature.vue";
-import Task from "../components/Task.vue";
+import Lane from "@/components/Lane.vue";
+import Epic from "@/components/Epic.vue";
+import Feature from "@/components/Feature.vue";
+import Task from "@/components/Task.vue";
+import ProjectCard from "@/components/ProjectCard.vue";
 
-import DetailView from '../components/DetailView.vue';
+import DetailView from '@/components/DetailView.vue';
 import DetailProject from '@/components/DetailProject.vue';
 
 
@@ -33,28 +34,31 @@ const routes = [
     component: Home,
   },
   {
-    path: "/ProductBacklog",
-    name: "ProductBacklog",
-    component: ProductBacklog
-  },{
-    path: "/SprintBacklog",
-    name: "SprintBacklog",
-    component: SprintBacklog
-  },
-  {
-    path: "/SprintPlaning",
-    name: "SprintPlaning",
-    component: SprintPlaning
-  },
-  {
-    path: "/Statistic",
-    name: "Statistic",
-    component: Statistic
-  },
-  {
-    path: "/Archive",
-    name: "Archive",
-    component: Archive
+    path: "/project/:id",
+    component: ProjectCard,
+    props: true,
+    children: [
+      {
+        path: "ProductBacklog",
+        component: ProductBacklog
+      },
+      {
+        path: "SprintBacklog",
+        component: SprintBacklog
+      },
+      {
+        path: "SprintPlaning",
+        component: SprintPlaning
+      },
+      {
+        path: "Statistic",
+        component: Statistic
+      },
+      {
+        path: "Archive",
+        component: Archive
+      },
+    ]
   },
   {
     path: "/Lane",
