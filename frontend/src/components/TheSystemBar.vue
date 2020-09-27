d8d63b55b18784bb5ac0300450baa24407ed2e1d
HD-Plus <dobehe78493@th-nuernberg.de>
2020-09-27 14:19:58 +0200

    First Fixes in Frontend

--- frontend/src/components/TheSystemBar.vue
+++ frontend/src/components/TheSystemBar.vue
@@ -1,28 +1,29 @@
 <template>
   <v-app-bar color="appbar" app dark dense flat>
-    <v-btn icon text color="appbar" :to="{name: 'Home'}">
+    <v-btn icon text color="appbar" :to="{ name: 'Home' }">
       <v-icon large color="link">mdi-home</v-icon>
     </v-btn>
-    
+
     <v-toolbar-title class="link--text">ScrumTool</v-toolbar-title>
 
+    <v-btn color="link" text x-small dark @click.stop="dialog = true"
+      >Benutzerverwaltung</v-btn
+    >
+    <PlattformUserManagement v-model="dialog" v-if="dialog" />
 
-    <v-btn
-      color="link"
-      text
-      x-small
-      dark
-      @click.stop="dialog = true"
-    >Benutzerverwaltung</v-btn>
-    <PlattformUserManagement v-model="dialog"/>
-    
     <v-spacer></v-spacer>
     <v-icon class="systemBarIcon">mdi-account</v-icon>
-    <span class="systemBarUser">{{userinfos.username}}</span>
+    <span class="systemBarUser">{{ userinfos.username }}</span>
     <v-btn icon text @click="logout()">
       <v-tooltip bottom color="link">
         <template v-slot:activator="{ on, attrs }">
-          <v-icon class="task-status-icons" color="link" v-bind="attrs" v-on="on">mdi-logout</v-icon>
+          <v-icon
+            class="task-status-icons"
+            color="link"
+            v-bind="attrs"
+            v-on="on"
+            >mdi-logout</v-icon
+          >
         </template>
         <span>Logout</span>
       </v-tooltip>
@@ -37,36 +38,31 @@ import { mapGetters } from "vuex";
 export default {
   data() {
     return {
-      dialog: false,
-
+      dialog: false
     };
   },
-  components:{
+  components: {
     PlattformUserManagement
   },
   methods: {
     logout() {
-      this.$store.dispatch('logout')
-        .then(() => {
-          this.$router.push('/login')
-        })
+      this.$store.dispatch("logout").then(() => {
+        this.$router.push("/login");
+      });
     },
-    
+
     initialize() {
       this.listSession();
-
-    },    
+    }
   },
 
   computed: {
-      ...mapGetters("session", {
-        listSession: "list"
-      }),
-      ...mapGetters(
-        {userinfos: "getUserinfo"
-      }),
+    ...mapGetters("session", {
+      listSession: "list"
+    }),
+    ...mapGetters({ userinfos: "getUserinfo" })
   },
-  mounted(){
+  mounted() {
     this.listSession;
   }
 };
