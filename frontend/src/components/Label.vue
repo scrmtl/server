a61fed66e1e0fc2f414cb46a47dcd912ac0fced3
HD-plus <dobehe78493@th-nuernberg.de>
2020-09-29 21:21:43 +0200

    show Backend labels

--- frontend/src/components/Label.vue
+++ frontend/src/components/Label.vue
@@ -10,17 +10,12 @@
       label="Labels"
       multiple
       chips
-      
       outlined
     >
       <template v-slot:no-data>
         <v-list-item>
           <span class="subheading">Create</span>
-          <v-chip
-            :color="`${colors[nonce - 1]} lighten-3`"
-            label
-            small
-          >
+          <v-chip :color="`${colors[nonce - 1]} `" label small>
             {{ search }}
           </v-chip>
         </v-list-item>
@@ -29,18 +24,16 @@
         <v-chip
           v-if="item === Object(item)"
           v-bind="attrs"
-          :color="`${item.color} lighten-3`"
+          :color="`${item.color} `"
           :input-value="selected"
           label
-          
         >
           <span class="pr-2">
             {{ item.title }}
           </span>
-          <v-icon
-            small
-            @click="parent.selectItem(item)"
-          >mdi-close-circle</v-icon>
+          <v-icon small @click="parent.selectItem(item)"
+            >mdi-close-circle</v-icon
+          >
         </v-chip>
       </template>
       <template v-slot:item="{ index, item }">
@@ -49,28 +42,18 @@
           v-model="editing.title"
           autofocus
           flat
-          
           background-color="transparent"
           hide-details
           outlined
           @keyup.enter="edit(index, item)"
         ></v-text-field>
-        <v-chip
-          v-else
-          :color="`${item.color} lighten-3`"
-          
-          label
-          small
-        >
+        <v-chip v-else :color="`${item.color}`" label small>
           {{ item.title }}
         </v-chip>
         <v-spacer></v-spacer>
         <v-list-item-action @click.stop>
-          <v-btn
-            icon
-            @click.stop.prevent="edit(index, item)"
-          >
-            <v-icon>{{ editing !== item ? 'mdi-pencil' : 'mdi-check' }}</v-icon>
+          <v-btn icon @click.stop.prevent="edit(index, item)">
+            <v-icon>{{ editing !== item ? "mdi-pencil" : "mdi-check" }}</v-icon>
           </v-btn>
         </v-list-item-action>
       </template>
@@ -80,81 +63,104 @@
 
 
 <script>
-  export default {
-    data: () => ({
-      activator: null,
-      attach: null,
-      colors: ['green', 'purple', 'indigo', 'cyan', 'teal', 'orange'],
-      editing: null,
-      index: -1,
-      items: [
-        { header: 'Select an label or create one' },
-        {
-          title: 'Foo',
-          color: 'blue',
-        },
-        {
-          title: 'Bar',
-          color: 'red',
-        },
-      ],
-      nonce: 1,
-      menu: false,
-      model: [
-        {
-          title: 'Foo',
-          color: 'blue',
-        },
-      ],
-      x: 0,
-      search: null,
-      y: 0,
-    }),
+import { mapActions, mapGetters } from "vuex";
+export default {
+  props: {
+    task: Object
+  },
+  data: () => ({
+    activator: null,
+    attach: null,
+    colors: ["green", "purple", "indigo", "cyan", "teal", "orange"],
+    editing: null,
+    fetchErrors: [],
+    index: -1,
+    items: [{ header: "Select an label or create one" }],
+    nonce: 1,
+    menu: false,
+    model: [],
+    x: 0,
+    search: null,
+    y: 0
+  }),
 
-    watch: {
-      model (val, prev) {
-        if (val.length === prev.length) return
+  watch: {
+    model(val, prev) {
+      if (val.length === prev.length) return;
 
-        this.model = val.map(v => {
-          if (typeof v === 'string') {
-            v = {
-              title: v,
-              color: this.colors[this.nonce - 1],
-            }
+      this.model = val.map(v => {
+        if (typeof v === "string") {
+          v = {
+            title: v,
+            color: this.colors[this.nonce - 1]
+          };
 
-            this.items.push(v)
+          this.items.push(v);
 
-            this.nonce++
-          }
-
-          return v
-        })
-      },
-    },
-
-    methods: {
-      edit (index, item) {
-        if (!this.editing) {
-          this.editing = item
-          this.index = index
-        } else {
-          this.editing = null
-          this.index = -1
+          this.nonce++;
         }
-      },
-      filter (item, queryText, itemText) {
-        if (item.header) return false
 
-        const hasValue = val => val != null ? val : ''
+        return v;
+      });
+    }
+  },
 
-        const title = hasValue(itemText)
-        const query = hasValue(queryText)
+  methods: {
+    ...mapActions("label", {
+      fetchLabels: "fetchList"
+    }),
+    edit(index, item) {
+      if (!this.editing) {
+        this.editing = item;
+        this.index = index;
+      } else {
+        this.editing = null;
+        this.index = -1;
+      }
+    },
+    filter(item, queryText, itemText) {
+      console.log("filter called; item: " + item + queryText + itemText);
+      if (item.header) return false;
 
-        return title.toString()
+      const hasValue = val => (val != null ? val : "");
+
+      const title = hasValue(itemText);
+      const query = hasValue(queryText);
+
+      return (
+        title
+          .toString()
           .toLowerCase()
           .indexOf(query.toString().toLowerCase()) > -1
-      },
+      );
     },
-
+    fetchAll() {
+      this.fetchLabels()
+        .catch(error => {
+          this.fetchErrors.push(error);
+        })
+        .then(() => {
+          this.listLabels.forEach(label => {
+            this.items.push(label);
+            this.model.push(label);
+          });
+        });
+      console.log("label IDs: " + this.getLabelIds(this.Task));
+    },
+    getLabelIds(Task) {
+      var labelIds = [];
+      Task.labels.forEach(label => {
+        labelIds.push(label.id);
+      });
+    }
+  },
+  computed: {
+    ...mapGetters("label", {
+      listLabels: "list"
+    })
+  },
+  created() {
+    this.fetchAll();
   }
+};
 </script>
