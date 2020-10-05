c4b772edb18ef3eb3afe59f234f736f9eb227248
HD-plus <dobehe78493@th-nuernberg.de>
2020-10-05 15:29:26 +0200

    Added customUrl to epic and feature

--- frontend/src/store/ressources/epic.js
+++ frontend/src/store/ressources/epic.js
@@ -14,20 +14,22 @@ export default createCrudModule({
     // list 
     // byId(id)
 
-    /**@description Custom function to get an array of epics
+    /** @description Custom function to get an array of epics
      * @param {number} laneId If set all epics in that lane are returned (exampleUrl: /api/lanes/1/epics`)
-     * @return {string} Url defined by the arguments
+     * @param {number} byUser Gets array of currently assigned epics of the authenticated user (example Url /api/epics/?byUser=1)
+     * @return {number} Url defined by the arguments
      */
-    customUrlFn(id, type, laneId = null) {
+    customUrlFn(id, type, { laneId = null, byUser = null }) {
 
         // id will only be available when doing request to single resource, otherwise null
         // type is the actions you are dispatching: FETCH_LIST, FETCH_SINGLE, CREATE, UPDATE, REPLACE, DESTROY
-        var rootUrl = '';
-        rootUrl = `/api/lanes/${laneId}/epics`;
-        rootUrl = id ? `${rootUrl}/${id}/` : rootUrl;
-
-        //const rootURL = `/api/epics/?lane=${laneId}`
-        //return rootUrl;
+        var rootUrl = id ? `/api/epics/${id}` : '/api/epics/';
+        if (laneId) {
+            rootURL = `/api/epics/?lane=${laneId}`
+        } else if (byUser) {
+            rootUrl = `/api/epics/?byUser=${byUser}`;
+            //rootURL = `/api/epics/?lane=${laneId}`
+        }
         return rootUrl;
-    }
+    },
 });
