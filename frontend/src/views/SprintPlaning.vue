77f7315dfe7f45c0e160d5b1b17ec4da76bbbfe3
huskyre <60154287+husKyRe@users.noreply.github.com>
2020-10-11 23:19:04 +0200

    add timeline for sprints and a lane for planning sprints
    only styling, no function

--- frontend/src/views/SprintPlaning.vue
+++ frontend/src/views/SprintPlaning.vue
@@ -3,6 +3,49 @@
     <v-col dense v-for="lane in neededPlanningLanes" :key="lane.numbering">
       <Lane v-bind:lane="lane"></Lane>
     </v-col>
+    <v-col>
+
+    </v-col>
+    <v-col>
+      <v-card min-width="385" max-width="420">
+        <v-card-title class="navbar white--text"> {{ selectedSprintName }}</v-card-title>
+        <v-card-text class="lane-body  flex-column" v-if="sprintLaneTask" >
+          <v-row justify="center"  class="" v-for="task in sprintLaneTask" :key="task.id">
+            <Task v-bind:task="task" />
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col>
+      
+      <v-timeline>
+        <v-timeline-item
+          small
+          fill-dot
+          color="tabbody"
+        >
+          <template v-slot:icon>
+            <v-btn fab small icon color="link" >
+              <v-icon>mdi-plus-circle-outline</v-icon>
+            </v-btn>
+          </template>
+        </v-timeline-item>
+        
+        <v-timeline-item
+          v-for="sprint in listSprints" :key="sprint.number"
+          small
+          fill-dot
+          
+        >
+          <template v-slot:icon>
+            <v-btn fab small color="link" class="white--text" >{{sprint.number}}</v-btn>
+            
+          </template>
+
+        </v-timeline-item>
+
+      </v-timeline>
+    </v-col>
   </v-row>
 </template>
 
@@ -11,7 +54,7 @@ import { mapGetters } from 'vuex';
 import Lane from "@/components/Lane.vue";
 export default {
   data: () => ({
-
+    selectedSprintName: "No sprint selected"
   }),
   components: {
     Lane
@@ -21,11 +64,14 @@ export default {
 
   },
   computed:{
-    ...mapGetters("board", {
-      boardByType: "byType"
+    ...mapGetters("task", {
+      tasksByIdArray: "byIdArray"
     }),
     ...mapGetters("lane", {
       laneByName: "byName"
+    }),
+    ...mapGetters("sprint", {
+      listSprints: "list"
     }),
 
     neededPlanningLanes(){
@@ -37,6 +83,10 @@ export default {
         lanes.push(readyLane.shift());
       }
       return lanes;
+    },
+
+    sprintLaneTask(){
+      return [];
     }
 
     
