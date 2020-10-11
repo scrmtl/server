1979447c6ef5d9fba57caea16106d2ceeb33f8a3
huskyre <60154287+husKyRe@users.noreply.github.com>
2020-10-11 15:35:54 +0200

    refactor boards vuex methods
    add the new url type

--- frontend/src/store/ressources/board.js
+++ frontend/src/store/ressources/board.js
@@ -15,17 +15,22 @@ export default createCrudModule({
     // byId(id))
 
     /** @description Custom function to get an array of boards
-     * @param {number} projectId If set all tasks in that lane are returned (exampleUrl: /api/projects/1/boards`)
+     * @param {number} projectId If set all boards in that project are returned 
      * @return {string} Url defined by the arguments
      */
     customUrlFn(id, type, projectId) {
         // id will only be available when doing request to single resource, otherwise null
         // type is the actions you are dispatching: FETCH_LIST, FETCH_SINGLE, CREATE, UPDATE, REPLACE, DESTROY
-        const rootUrl = `/api/projects/${projectId}/boards`;
-        //const rootURL = `/api/boards/?project=${projectId}`
-        //return rootUrl;
-        return id ? `${rootUrl}/${id}/` : rootUrl;
+        var rootUrl = '/api/boards';
+        if (projectId != null) {
+            rootUrl = `/api/boards/?projects=${projectId}`
+        }
+        rootUrl = id ? `${rootUrl}/${id}/` : rootUrl;
+        return rootUrl;
+
     },
+
+
     getters: {
         /** @description Add Custom getter 
          * @param {string} type Board Type (PB, SP, AB) 
