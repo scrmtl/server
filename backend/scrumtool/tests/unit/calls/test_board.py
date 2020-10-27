d4c41030a9be0c8da0dcdd1e22aeb80574a3171e
Michael Merbach <mmerbach1@web.de>
2020-10-27 21:04:50 +0100

    Added first test core

---
+++ backend/scrumtool/tests/unit/calls/test_board.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from api.models import Board
+
+factory = APIRequestFactory()
+request = factory.post('/notes/', {'title': 'new idea'})
