f7cf717af7f82a5fb5113813fbe5b5c7139da1d6
Dreamer <dreamer@online.de>
2020-11-07 22:07:00 +0100

    Erste Arbeiten an der Statistik...

--- frontend/src/views/Archive.vue
+++ frontend/src/views/Archive.vue
@@ -1,12 +1,65 @@
-
 <template>
   <v-main class="tabbody tab-content">
-    Archive - View
+    <v-col>
+      
+      <v-timeline>
+        <v-timeline-item
+          small
+          fill-dot
+          color="tabbody"
+        >
+          <template v-slot:icon>
+            <v-btn fab small icon color="link" >
+              <v-icon>mdi-plus-circle-outline</v-icon>
+            </v-btn>
+          </template>
+        </v-timeline-item>
+        
+        <v-timeline-item
+          v-for="sprint in listSprints" :key="sprint.number"
+          small
+          fill-dot
+          
+        >
+          <template v-slot:icon>
+            <v-btn fab small color="link" class="white--text" >{{sprint.number}}</v-btn>
+            
+          </template>
+
+        </v-timeline-item>
+
+      </v-timeline>
+    </v-col>
+    <v-col>
+      <!-- Hier kommt dann das weitere... /-->
+      <v-text>
+        Hier kommt der Graph
+      </v-text> 
+    </v-col>
+    <v-col>
+      Hier die 4 Statisik-Werte
+    </v-col>
   </v-main>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from 'vuex';
+export default {
+  data: () => ({
+    selectedSprintName: "No sprint selected"
+  }),
+  components: {
+  },
+  methods:{
+    
+
+  },
+  computed:{
+    ...mapGetters("sprint", {
+      listSprints: "list"
+    }),
+  }
+};
 </script>
 
 <style lang="css" scoped>
