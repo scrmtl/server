d530d568e3d959e4090222081d3590a4d70f1682
HD-plus <dobehe78493@th-nuernberg.de>
2020-12-05 12:44:28 +0100

    fixed bugs in apscheduler

--- backend/scrumtool/api/management/commands/runapscheduler.py
+++ backend/scrumtool/api/management/commands/runapscheduler.py
@@ -18,19 +18,26 @@ logger = logging.getLogger(__name__)
 
 
 def my_job():
-    #  Your job processing logic here...
+    logger.info("---------------------Executing my_job-----------------------")
+    logger.info(
+        f"Sprints today: {Sprint.objects.filter(start=date.today()).all()}")
     set_sprint_in_progress_handler(
-        Sprint.objects.filter(start=date.today()))
-    set_sprint_done_handler(Sprint.objects.filter(end=date.today()))
-    set_sprint_accepted_handler(Sprint.objects.filter(end=date.today()))
+        Sprint.objects.filter(start=date.today()).all())
+    set_sprint_done_handler(Sprint.objects.filter(end=date.today()).all())
+    set_sprint_accepted_handler(Sprint.objects.filter(end=date.today()).all())
     move_cards_handler(
-        Sprint.objects.filter(start=date.today()))
+        Sprint.objects.filter(start=date.today()).all())
 
 
 def set_sprint_in_progress_handler(sprints_today):
     sprint: Sprint
     for sprint in sprints_today:
-        if sprint.status == Sprint.SprintStatus.PLANNED:
+
+        if (sprint.status == Sprint.SprintStatus.PLANNED) and \
+                (len(sprint.task_cards.all()) > 0):
+            logger.info(
+                f"Set status {Sprint.SprintStatus.IN_PROGRESS}\
+                     in Sprint {sprint}")
             sprint.status = Sprint.SprintStatus.IN_PROGRESS
             sprint.save()
 
@@ -39,14 +46,26 @@ def set_sprint_done_handler(sprints_today):
     sprint: Sprint
     for sprint in sprints_today:
         if sprint.status == Sprint.SprintStatus.IN_PROGRESS:
+            logger.info(
+                f"Set status {Sprint.SprintStatus.DONE}\
+                     in Sprint {sprint}")
             sprint.status = Sprint.SprintStatus.DONE
             sprint.save()
 
 
 def set_sprint_accepted_handler(sprints_today):
     sprint: Sprint
+    task: Task
+    all_tasks_accepted = True
     for sprint in sprints_today:
-        if sprint.status == Sprint.SprintStatus.DONE:
+        for task in sprint.task_cards.all():
+            if task.status != Task.Status.ACCEPTED:
+                all_tasks_accepted = False
+        if sprint.status == Sprint.SprintStatus.DONE and all_tasks_accepted:
+
+            logger.info(
+                f"Set status {Sprint.SprintStatus.ACCEPTED}\
+                     in Sprint {sprint}")
             sprint.status = Sprint.SprintStatus.ACCEPTED
             sprint.save()
 
@@ -54,25 +73,40 @@ def set_sprint_accepted_handler(sprints_today):
 def move_cards_handler(sprints_today):
     sprint: Sprint
     for sprint in sprints_today:
-
+        logger.info(
+            f"--> Sprint today: {sprint}")
         if sprint.status == Sprint.SprintStatus.IN_PROGRESS:
             # get lane of PB
+            logger.info(
+                f"--> Sprint today: {sprint.project.boards}")
             pb_board: Board = sprint.project.boards.get(
                 board_type=Board.BoardType.PB)
+            logger.info(
+                f"   --> PB : {pb_board}")
             sb_board: Board = sprint.project.boards.get(
-                board_type=Board.BoardType.SB)
+                board_type=Board.BoardType.SP)
+            logger.info(
+                f"   --> SB : {sb_board}")
             growing_lane: Lane = pb_board.lanes.get(name__icontains='Growning')
             next_lane: Lane = sb_board.lanes.get(name__icontains='Next')
+            logger.info(
+                f"Move cards from lane {growing_lane}\
+                     located in board {pb_board} to lane\
+                     {next_lane} located in board {sb_board}")
             # get tasks of PB
             tasks = Task.objects.filter(lane=growing_lane.id)
             task: Task
             for task in tasks.all():
-                task.lane = next_lane
-                task.save()
+                if task.sprint is not None:
+                    logger.info(
+                        f"Moved card {task}")
+                    task.lane = next_lane
+                    task.save()
 
 
 def midnight_job():
-    #  Your job processing logic here...
+    # This job actually starts at 10 o'clock because the server aka
+    # the old laptop is only online from 8:00-22:00
     pass
 
 
@@ -90,7 +124,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(second="*/10"),  # Every 10 seconds
+            trigger=CronTrigger(second="*/59"),  # Every 10 seconds
             id="my_job",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
