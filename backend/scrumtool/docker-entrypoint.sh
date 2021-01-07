a90c0700cd6a3dff9989f13991b7ab4ab54a3871
HD-plus <dobehe78493@th-nuernberg.de>
2021-01-08 00:20:48 +0100

    Prevent Django launching two Threads

--- backend/scrumtool/docker-entrypoint.sh
+++ backend/scrumtool/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 echo "execute python manage.py migrate "
 python manage.py migrate
 echo "execute manage.py runserver 0.0.0.0:8000 "
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 --noreload
 
 exec "$@"
 
