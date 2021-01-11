e5ea4988295cc11ffd24a197797be6b0a8dc199e
HD-Plus <dobehe78493@th-nuernberg.de>
2021-01-11 14:15:19 +0100

    Django should only start once

--- backend/scrumtool/docker-entrypoint.sh
+++ backend/scrumtool/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 
-echo "execute python manage.py migrate "
-python manage.py migrate
 echo "execute manage.py runserver 0.0.0.0:8000 "
 python manage.py runserver 0.0.0.0:8000 --noreload
 
