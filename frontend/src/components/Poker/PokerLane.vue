9707e7e266ea451d45a701c5cabbdfaca251c58c
huskyre <60154287+husKyRe@users.noreply.github.com>
2021-01-17 19:20:38 +0100

    Add async planning poker to Sprint planning View #26
    add backend api call to create async planning pokers
    add Dialog for async planning pokers
    add option for open end poker or timeboxed option

--- frontend/src/components/Poker/PokerLane.vue
+++ frontend/src/components/Poker/PokerLane.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
 <v-card class="primary" min-width="300" max-width="400">
   <v-card-title class="white--text">
       Gromming
@@ -25,10 +26,10 @@
       <v-row>
         <v-subheader class="white--text text-body-1">Cards to vote</v-subheader>
         <v-list two-line class="transparent overflow-y-auto" style="max-height: calc(65vh - 225px)">
-          <v-list-item v-for="item in voteCards" :key="item.id">
+          <v-list-item v-for="item in cardsToVote" :key="item.cardId">
           <v-list-item-content>
-            <v-list-item-title class="white--text">#{{item.id}}: {{item.name}}</v-list-item-title>
-            <v-list-item-subtitle class="white--text">Storypoints: {{item.storypoints}}</v-list-item-subtitle>
+            <v-list-item-title class="white--text">#{{item.cardId}}: {{item.cardName}}</v-list-item-title>
+            <v-list-item-subtitle class="white--text">Storypoints: {{item.cardStorypoints}}</v-list-item-subtitle>
           </v-list-item-content>
           <v-list-item-action>
             <v-btn icon>
@@ -38,35 +39,125 @@
           </v-list-item>
         </v-list>
       </v-row>
+      <v-row>
+        <v-subheader class="white--text text-body-1">Options</v-subheader>
+      </v-row>
+      <v-row no-gutters align="center">
+        <v-col cols="6">
+          <v-checkbox label="Open End Vote" v-model="openEndVote" value color="link" dark></v-checkbox>
+        </v-col>
+        <v-col cols="6">
+          <v-menu
+            ref="voteDateMenu"
+            v-model="VoteDateMenu"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="endVoteDate"
+                label="End Vote date"
+                prepend-icon="mdi-calendar"
+                readonly
+                dark
+                :disabled="openEndVote"
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              ref="picker"
+              v-model="endVoteDate"
+              :min="new Date().toISOString()"
+              @change="saveEndVoteDate"
+            ></v-date-picker>
+          </v-menu>
+        </v-col>
+      </v-row>
     </v-card-text>
     <v-card-subtitle class="white--text text-body-1">
       Start Poker
     </v-card-subtitle>
     <v-card-actions>
-      <v-btn outlined color="link">Poker Sync</v-btn>
+      <v-btn outlined disabled color="link">Poker Sync</v-btn>
       <v-spacer></v-spacer>
-      <v-btn outlined color="link">Poker Async</v-btn>
+      <v-btn outlined color="link" :disabled="!allowAsyncPoker" @click="asyncPokerDialog=true">Poker Async</v-btn>
     </v-card-actions>
 </v-card>
-  
+<!-- Sprint Release Dialog -->
+  <v-dialog
+    v-model="asyncPokerDialog"
+    persistent
+    class="mx-auto"
+    width="600"
+    dark
+  >
+    <v-card color="tabbody" shaped>
+      <v-card-text class="headline pt-10">
+        <span class="ml-12">Do you want to start a Async Planning Poker?</span>
+      </v-card-text>
+      <v-card-actions class="ml-10 pb-10 pt-10">
+        <v-btn width="250" outlined  @click="startAsyncPoker()"
+          >Yes</v-btn
+        >
+        <v-btn
+          width="250"
+          outlined
+          @click="asyncPokerDialog = false"
+          >No</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import { mapFields } from "vuex-map-fields";
 export default {
   name: "PokerLane",
   data: () =>({
-
+    asyncPokerDialog: false,
+    VoteDateMenu: false,
+    openEndVote: true,
+    endVoteDate: null,
+    cardsToVote:[]
   }),
   computed: {
     // See more under Two-way Computed Property https://vuex.vuejs.org/guide/forms.html
     // Implementation with https://github.com/maoberlehner/vuex-map-fields
     // the string after the last dot (e.g. `id`) is used
     // for defining the name of the computed property.
-    ...mapFields("poker", ["voteCards"]),
-
+    ...mapFields([
+      "Userinfo.userId"
+    ]),
+    allowAsyncPoker(){
+      var allowed = false;
+        if(this.cardsToVote.length > 0 && this.openEndVote){
+          allowed = true;
+        }
+        else if(this.cardsToVote.length > 0 && !this.openEndVote && this.endVoteDate !== null){
+          allowed = true;
+        }
+        else{
+          allowed = false;
+        }
+      return allowed;
+    }
   },
   methods: {
+    ...mapActions("pokerVoting", {
+      fetchSingleAsyncPokerVoting: "fetchSingle",
+      createAsyncPokerVoting: "create",
+      deleteAsyncPokerVoting: "destroy",
+    }),
+    ...mapActions("pokerVote", {
+      fetchSingleAsyncPokerVote: "fetchSingle",
+      createAsyncPokerVote: "create",
+    }),
     allowDrop(e){
       const taskStatus = e.dataTransfer.getData("task-status");
       const sprintStatus = e.dataTransfer.getData("task-sprint-status");
@@ -88,22 +179,78 @@ export default {
       // const taskSprintId = e.dataTransfer.getData("task-sprint-id");
       // const taskSprintStatus = e.dataTransfer.getData("task-sprint-status");
       const taskStorypoints = e.dataTransfer.getData("task-storypoints");
-      if(this.voteCards.findIndex(item => item.id == taskId) == -1){
-        this.$store.commit("poker/addCardToVote", {cardName: taskName , cardId: taskId, cardStorypoints: taskStorypoints});
+      if(this.cardsToVote.findIndex(item => item.cardId == taskId) == -1){
+        this.cardsToVote.push({cardName: taskName , cardId: taskId, cardStorypoints: taskStorypoints});
       }
       else{
         this.$store.commit("showSystemAlert", {
-        message: "Card is already added in poker",
+        message: "Card is already added in planning poker",
         category: "warning"
         });
       }
       
     },
     removePoker(vote){
-      this.$store.commit("poker/removeCardToVote", vote.id);
+      this.cardsToVote = this.cardsToVote.filter(item => item.cardId !== vote.cardId);
+    },
+    saveEndVoteDate(date){
+      this.$refs.voteDateMenu.save(date);
+    },
+    startAsyncPoker(){
+      this.asyncPokerDialog = false;
+      var asyncPokerData = {
+        project: this.$route.params.id,
+        modus: "ASYNC",
+        manager: this.userId,
+        voters: []
+      }
+      if(this.openEndVote){
+        asyncPokerData.start = new Date().toISOString().slice(0,10);
+        asyncPokerData.end = null;
+      }
+      else{
+        asyncPokerData.start = new Date().toISOString().slice(0,10);
+        asyncPokerData.end = this.endVoteDate;
+      }
+      console.log(asyncPokerData);
+      this.createAsyncPokerVoting({
+        data: asyncPokerData
+      }).then((res) => {
+        console.log(res)
+        // Create pokerVotes belongs to the created pokerVoting
+        const pokerVotingId = res.data.id;
+        if(pokerVotingId !== null){
+          this.cardsToVote.forEach(card => {
+            this.createAsyncPokerVote({
+              data:{
+                poker_voting: pokerVotingId,
+                task: card.cardId,
+                status: "NS"
+              }
+            }).catch((error)=>{
+              console.log(error);
+              console.log(pokerVotingId);
+              this.deleteAsyncPokerVoting({id: pokerVotingId})
+              this.$store.commit("showSystemAlert", {
+                message: "Async planning poker with one of those cards already exist." + error.data.non_field_errors[0],
+                category: "error"
+              });
+
+            });
+          });
+          // delete list with card to vote
+          this.cardsToVote = [];
+        }
+        else{
+          this.$store.commit("showSystemAlert", {
+          message: "Can't create async planning poker",
+          category: "error"
+          });
+        }
+      })
     }
 
-  }
+  },
 }
 </script>
 
