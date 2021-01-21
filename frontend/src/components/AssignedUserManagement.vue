9acb598b4b983912d0e058d4c4b5041bb9172cac
Dreamer <dreamer@online.de>
2021-01-21 19:58:43 +0100

    Delete Project User Dialog hinzugefügt

--- frontend/src/components/AssignedUserManagement.vue
+++ frontend/src/components/AssignedUserManagement.vue
@@ -1,13 +1,13 @@
 <template>
   <v-dialog
     v-model="visibleDialog"
-    scrollable  
+    scrollable
     persistent
     max-width="1200"
     transition="dialog-transition"
   >
     <v-card color="tabbody" dark>
-      <v-card-title> {{dialogName}} </v-card-title>
+      <v-card-title> {{ dialogName }} </v-card-title>
       <v-card-text>
         <v-data-table
           :headers="headers"
@@ -18,7 +18,7 @@
           dark
         >
           <template v-slot:top>
-            <v-row >
+            <v-row>
               <v-col>
                 <v-autocomplete
                   outlined
@@ -34,16 +34,20 @@
                 >
                   <template v-slot:item="data">
                     <v-list-item-content>
-                      <v-list-item-title v-html="data.item.username"></v-list-item-title>
-                      <v-list-item-subtitle v-html="data.item.name"></v-list-item-subtitle>
+                      <v-list-item-title
+                        v-html="data.item.username"
+                      ></v-list-item-title>
+                      <v-list-item-subtitle
+                        v-html="data.item.name"
+                      ></v-list-item-subtitle>
                     </v-list-item-content>
                   </template>
                 </v-autocomplete>
               </v-col>
-              
+
               <v-col>
-                <v-btn 
-                  outlined 
+                <v-btn
+                  outlined
                   color="link"
                   text
                   @click="addAssignedUser(selectedUser)"
@@ -53,8 +57,6 @@
                 </v-btn>
               </v-col>
             </v-row>
-            
-            
           </template>
           <template v-slot:[`item.role`]="{ item }">
             <v-select
@@ -64,74 +66,89 @@
               @change="updateRole($event, item)"
             ></v-select>
           </template>
-          <template v-slot:[`item.actions`]="{item}">
-            <v-icon
-              small
-              @click="removeAssignedUser(item)"
-            >
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small @click="removeUser(item)">
               mdi-delete
             </v-icon>
           </template>
         </v-data-table>
-        
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn 
-          color="link" 
-          outlined 
-          small 
-          @click="close">
-          CLOSE
-        </v-btn>
+        <v-btn color="link" outlined small @click="close"> CLOSE </v-btn>
       </v-card-actions>
     </v-card>
-    
-  </v-dialog>
 
+    <!-- Delete Dialog -->
+    <v-dialog
+      v-model="deleteDialog"
+      persistent
+      class="mx-auto"
+      width="600"
+      dark
+    >
+      <v-card color="tabbody" shaped>
+        <v-card-text class="headline pt-10">
+          <span class="ml-12"
+            >Remove the user form the project?</span
+          >
+        </v-card-text>
+        <v-card-actions class="ml-10 pb-10 pt-10">
+          <v-btn width="250" outlined color="error" @click="removeAssignedUser()"
+            >Yes</v-btn
+          >
+          <v-btn width="250" outlined @click="deleteDialog = false">No</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-dialog>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "AssignedUserManagement",
   props: {
     dialog: { type: Boolean, default: false },
     dialogName: { type: String, default: "Assigned user" },
-    color: { type: String, default: ""},
+    color: { type: String, default: "" },
     roleEditing: { type: Boolean, default: false },
-    assignedUsers: {type: Array},
-    availableUsers: {type: Array},
+    assignedUsers: { type: Array },
+    availableUsers: { type: Array },
   },
   data: () => ({
     headers: [
       { text: "Benutzername", value: "plattform_user.username" },
       { text: "Name", value: "plattform_user.name" },
       { text: "Role", value: "role" },
-      { text: "Actions", value: "actions", sortable: false }
+      { text: "Actions", value: "actions", sortable: false },
     ],
     availableRoles: ["product owner", "developer", "scrum master"],
-    selectedUser: {}
+    selectedUser: {},
+    deleteDialog: false,
+    deleteUser: null,
   }),
-  components:{
-  },
+  components: {},
 
-  methods:{
+  methods: {
     ...mapActions("projectUser", {
       fetchUser: "fetchList",
-      updateProjectUser: "update"
+      updateProjectUser: "update",
     }),
     ...mapActions("projectRole", {
-      fetchRoles: "fetchList"
+      fetchRoles: "fetchList",
     }),
-    
+
     close() {
       this.$emit("close-dialog");
     },
-    removeAssignedUser(user){
-      this.$emit("remove-user", user.id);
+    removeAssignedUser() {
+      if (this.deleteUser !== null) {
+        console.log(this.deleteUser.id);
+        this.$emit("remove-user", this.deleteUser.id);
+      }
     },
-    addAssignedUser(user){
+    addAssignedUser(user) {
       this.$emit("add-user", user);
       this.selectedUser = {};
     },
@@ -139,21 +156,26 @@ export default {
       this.updateProjectUser({
         id: item.id,
         data: { role: this.byRoleName(newRole) },
-        customUrlFnArgs: {}
+        customUrlFnArgs: {},
       });
     },
-    nameUsernameFilter (item, queryText) {
-        const textOne = item.name.toLowerCase()
-        const textTwo = item.username.toLowerCase()
-        const searchText = queryText.toLowerCase()
-        return textOne.indexOf(searchText) > -1 ||
-          textTwo.indexOf(searchText) > -1
+    nameUsernameFilter(item, queryText) {
+      const textOne = item.name.toLowerCase();
+      const textTwo = item.username.toLowerCase();
+      const searchText = queryText.toLowerCase();
+      return (
+        textOne.indexOf(searchText) > -1 || textTwo.indexOf(searchText) > -1
+      );
     },
 
+    removeUser(user) {
+      this.deleteUser = user;
+      this.deleteDialog = true;
+    },
   },
-  computed:{
-    ...mapGetters("projectRole",{
-        byRoleName: "byName"
+  computed: {
+    ...mapGetters("projectRole", {
+      byRoleName: "byName",
     }),
     visibleDialog: {
       get() {
@@ -165,12 +187,12 @@ export default {
         } else {
           this.$emit("close-dialog");
         }
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
 </script>
 
 <style lang="css" scoped>
-  @import "../main.css";
+@import "../main.css";
 </style>
