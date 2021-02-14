c1fa4af3fae3ad8a2fc63a8c192d1f990838577f
HD-plus <dobehe78493@th-nuernberg.de>
2021-02-14 21:41:15 +0100

    log for task signal

--- backend/scrumtool/api/signals/task_signals.py
+++ backend/scrumtool/api/signals/task_signals.py
@@ -4,14 +4,22 @@ from api.models.card import Task, Epic, Feature
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 @receiver(pre_save, sender=Task)
 def set_done_on_task(sender, instance: Task, update_fields, **kwargs):
+    logger.info(f'---------------task signal----------------')
     if not Task.objects.filter(id=instance.id).exists():
         return
     old_instance = Task.objects.get(id=instance.id)
+    logger.info(f'new status of task {instance} is {instance.status}')
+    logger.info(f'old status of task {old_instance} is {old_instance.status}')
     if (instance.status == Task.Status.DONE and
             old_instance.status != Task.Status.DONE):
+        logger.info(f'overwrite task date_on with {date.today()}')
         instance.done_on = date.today()
     elif instance.status == Task.Status.ACCEPTED:
         return
